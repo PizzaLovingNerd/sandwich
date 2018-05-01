d01b52c04485d8d8590a66c61ff6eb6b0a9f964c
PizzaLovingNerd <pizzalovingnerd@protonmail.com>
2018-05-01 16:47:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# NOTE: Do NOT change if statement to /usr/local/bin/sandwich. This is to remove a build that was installed by an older version of the installer.
-if [ -e /usr/bin/sandwich ];
-then
-  echo "Old build of sandwich found. Removing it ..."
-  sudo rm -rf /usr/bin/sandwich
-fi
-
 echo ">>> setting up directories"
 rm -rf ~/.sandwich/ # remove old config directories
 sudo mkdir /usr/local/share/sandwich # create new config directory (will only show warning if it already exists)
