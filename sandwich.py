cf8f56763997546ada2f643c82c9c2726e1a37ce
PizzaLovingNerd <pizzalovingnerd@protonmail.com>
2018-05-01 11:51:25 -0700

    Update sandwich.py

--- sandwich.py
+++ sandwich.py
@@ -2,5 +2,10 @@ import os
 
 if os.geteuid() == 0:
     print("\nGo make me a sandwich")
-    print("Okay Boss")
-    print("\nYou are logged in as root\n")
+    print("\nOkay Boss")
+    print("\nYou are logged in as root or you are using sudo\n")
+else:
+    print("\nGo make me a sandwich")
+    print("\nYour not the boss of me, make yourself a sandwich")
+    print("\nYou are not logged in as root or you are not using sudo\n")
+    
