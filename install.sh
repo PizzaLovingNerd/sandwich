c8a722ad6bba5d24c22180fde34f13580c2ae52f
PizzaLovingNerd <pizzalovingnerd@protonmail.com>
2018-05-11 12:19:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
+echo "Installing"
 
-echo ">>> setting up directories"
-rm -rf ~/.sandwich/ # remove old config directories
-sudo mkdir /usr/local/share/sandwich # create new config directory (will only show warning if it already exists)
-echo ">>> copying program to /usr/local/bin"
-sudo cp sandwich.py /usr/local/bin/ # copy program to PATH (will only show warning if it needs to overwrite)
-echo ">>> installing"
-chmod +x sandwich
-sudo cp sandwich /usr/local/bin/ # copy link program to /usr/local/bin
-echo ""
-echo ">>> Done!"
+chmod +x sandwich.py
+
+sudo cp sandwich.py /usr/local/bin/sandwich
+
+echo "Done"
